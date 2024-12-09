dce687403dd0633eb5b7e685efa3049f3281a830
Zer0Hiro <95877340+Zer0Hiro@users.noreply.github.com>
2024-12-09 14:33:07 +0200

    Cleaner code

--- Submition 1/sub2v1.py
+++ Submition 1/sub2v1.py
@@ -15,30 +15,29 @@ def main():
     firstnum, secondnum = eval(input("Insert two numbers in base 7: "))
     #Start Checker
     while num_check != 1:
-        while num_check != 1:
-            #Get first units from each number
-            firstnum_storage = firstnum%10
-            secondnum_storage = secondnum%10
-            #Count sum of units of both numbers + 1 if previous units were >=7
-            num_sum = (firstnum_storage + secondnum_storage) + not_recorded
-            #Reset recorder for next loop
-            not_recorded = 0
-            #Remove units from number, prepare them for next loop
-            firstnum //= 10
-            secondnum //= 10
-            #Condition checker
-            if firstnum_storage > 6 or secondnum_storage > 6:
-                num_check = 1
-                print("Illegal input!")
+        #Get first units from each number
+        firstnum_storage = firstnum%10
+        secondnum_storage = secondnum%10
+        #Count sum of units of both numbers + 1 if previous units were >=7
+        num_sum = (firstnum_storage + secondnum_storage) + not_recorded
+        #Reset recorder for next loop
+        not_recorded = 0
+        #Remove units from number, prepare them for next loop
+        firstnum //= 10
+        secondnum //= 10
+        #Condition checker
+        if firstnum_storage > 6 or secondnum_storage > 6:
+            num_check = 1
+            print("Illegal input!")
+        else:
+            #Group of 7 checker
+            if num_sum >= 7:
+                finalnum = num_sum%7
+                not_recorded = 1
             else:
-                #Group of 7 checker
-                if num_sum >= 7:
-                    finalnum = num_sum%7
-                    not_recorded = 1
-                else:
-                    finalnum = num_sum
-                true_num = finalnum*(10**num_count) + true_num
-                num_count += 1     
+                finalnum = num_sum
+            true_num = finalnum*(10**num_count) + true_num
+            num_count += 1     
             #End of loop
             if firstnum == 0 and secondnum == 0:
                 #If last loop summ >= 7 add additional number
