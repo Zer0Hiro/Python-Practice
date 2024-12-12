2681c810cde24a8d66ef36623395c641439c2870
Zer0Hiro <95877340+Zer0Hiro@users.noreply.github.com>
2024-12-12 20:45:38 +0200

    Fixed

--- SomeHelp/practice1fixed.py
+++ SomeHelp/practice1fixed.py
@@ -6,39 +6,43 @@ Created on Tue Dec 10 15:53:23 2024
 """
 
 def main():
-    s_delta=0
+    length=1
     delta=0
-    delta_final=0
-    length=0
-    s_length=1
-    start = 0
+    delta_s=0
+    delta_f=0
+    length_s=0
+    start=0
+    count = 0
     num1=int(input("Insert 1st number:"))
-    start_s = num1
+    start=num1
     while num1!=-1 and num1>0:
         num2=int(input("Insert next number, end with -1:"))
         if num2==-1 or num2<0: 
             num1=num2
         else:
-            s_delta=num2-num1
-            s_length+=1
-            if s_delta!=delta:
-                if s_length>length:
-                    delta_final=delta
-                    start_s = start
-                    length=s_length-1
-                s_length=2
-                start = num1
-            delta=s_delta
+            length+=1
+            delta=num2-num1
+            if delta!=delta_s:
+                if length>length_s:
+                    start_s=start
+                    delta_f=delta_s
+                    length_s=length-1
+                length=2
+                start=num1
+                delta_s=delta
+            count+=1
             num1=num2
     
-    if s_length<=length:
-        start = start_s
-        s_length = length
-        delta = delta_final
-        
-        
+    if length<=length_s:
+        delta=delta_f
+        start=start_s
+        length=length_s
+             
     if num1==-1:
-        print("Longest mathematical series start with %d, delta of %d, length of %d"%(start,delta_final,s_length))
+        if count==0:
+            print("Longest mathematical series start with %d, length of %d"%(start,length))
+        else:
+            print("Longest mathematical series start with %d, delta of %d, length of %d"%(start,delta,length))
     else:
         print("Illegal data!")
 main()
