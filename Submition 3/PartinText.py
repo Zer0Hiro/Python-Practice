8ecdc08956835d0752fa1ff0d748b5867dd9e47d
Zer0Hiro <95877340+Zer0Hiro@users.noreply.github.com>
2025-01-04 21:55:17 +0200

    Create PartinText.py

---
+++ Submition 3/PartinText.py
@@ -0,0 +1,37 @@
+#Dmitry Chybisov 342744869
+#Yelyzaveta Kytaieva 342794039
+#Counts how many times requested part(part) appears in text
+def count_occurrences(text, part):
+    count = 0
+    l = len(part) 
+    for i in range(len(text)):
+        #Check if part that we need equal to part in text
+        if part == text[i:i+l]:
+            count += 1     
+    return count
+
+#Counts which part from the list (lst) appears most of the time    
+def max_occurrences(text,lst):
+    maximum = 0
+    index = 0
+    for x in range(len(lst)):
+       count = count_occurrences(text, lst[x])
+       #Storage
+       if count > maximum:
+           maximum = count
+           index = x      
+    return lst[index] 
+
+#Main function, gets main string and amount of parts at the end shows which part appears most of the time
+def main():
+    string = input("Enter a string: ")
+    amount = int(input("Enter the list length: "))
+    parts = []  
+    #Creates list of parts
+    print("Enter %d strings:"%amount)
+    for i in range(amount):
+        parts.append(input())
+    #Output of the final calculations
+    print('The most repeated string is "%s"'%max_occurrences(string,parts))
+    
+main()
