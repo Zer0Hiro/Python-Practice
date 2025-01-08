d3bb34768bf256e38f4814f2549e06dacb2fda68
Zer0Hiro <95877340+Zer0Hiro@users.noreply.github.com>
2025-01-08 13:36:49 +0200

    Update New Lines.py

--- Practice week 8/New Lines.py
+++ Practice week 8/New Lines.py
@@ -1,3 +1,4 @@
+#Removes any spaces from string and writes each word as a new line
 def no_space(msg):
     for i in range(len(msg)):
         if msg[i] == " ":
@@ -5,7 +6,9 @@ def no_space(msg):
         else:
             print(msg[i], end="")
     print()
- 
+
+#Gets the string and check if its empty,
+#If not call for no_space()
 def main():
     n = 0
     while n == 0:
