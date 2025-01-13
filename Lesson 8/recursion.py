9041636717d9752463edae62de6f8641f98e6a9d
Zer0Hiro <95877340+Zer0Hiro@users.noreply.github.com>
2025-01-13 09:43:05 +0200

    Added main to recursion.py

--- Lesson 8/recursion.py
+++ Lesson 8/recursion.py
@@ -2,3 +2,9 @@ def sum_num(num):
     if num < 10:
         return num
     return num%10 + sum_num(num//10)
+
+def main():
+    num = int(input("Enter a number: "))
+    print(sum_num(num))
+
+main()
