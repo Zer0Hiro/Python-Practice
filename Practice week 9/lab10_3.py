21f8ded6d0cfda924f932e1d804676656bac7525
Zer0Hiro <95877340+Zer0Hiro@users.noreply.github.com>
2025-01-16 14:17:44 +0200

    Update lab10_3.py

--- Practice week 9/lab10_3.py
+++ Practice week 9/lab10_3.py
@@ -1,19 +1,21 @@
-#Search for the lowest number in the list
-#Using binary search
-def minShiftedList(lst,length):
+# Search for the lowest number in the list
+# Using binary search
+def minShiftedList(lst, length):
     l = length
     left, right = 0, l - 1
     while l != 0:
-        mid = (left + right)//2
+        mid = (left + right) // 2
         if lst[mid] > lst[left]:
             left = mid
         elif lst[mid] < lst[left]:
             right = mid
         else:
-            #+1 because mid will be equal to the biggest number in the list
-            return lst[mid+1]
-#Creates list of numbers
-#Print the smallest number in the list
+            # +1 because mid will be equal to the biggest number in the list
+            return lst[mid + 1]
+
+
+# Creates list of numbers
+# Print the smallest number in the list
 def main():
     n = 0
     lst = []
@@ -24,6 +26,7 @@ def main():
             n = 1
         else:
             lst.append(a)
-    print("The smallest number is:",minShiftedList(lst,len(lst)))
+    print("The smallest number is:", minShiftedList(lst, len(lst)))
+
 
 main()
