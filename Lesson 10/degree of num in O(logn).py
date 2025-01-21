1d916428f79a75304e05094bb9dd9c573f0c8604
Zer0Hiro <95877340+Zer0Hiro@users.noreply.github.com>
2025-01-21 12:05:45 +0200

    Create degree of num in O(logn).py

---
+++ Lesson 10/degree of num in O(logn).py
@@ -0,0 +1,15 @@
+def power_rec(a, n):
+    if n == 1:
+        return a
+    elif n == 0:
+        return 1
+    z = power_rec(a, n // 2)
+    if (n//2)%2 == 0:
+        return z*z
+    else:
+        return z*z*a
+
+
+print(power_rec(2,27))
+
+
