3dcf18073cbd21e32af3fa39b7df028e6ccb3c1d
Zer0Hiro <95877340+Zer0Hiro@users.noreply.github.com>
2025-01-21 10:34:59 +0200

    Fibanachi_0(n).py still doesnt work

---
+++ Lesson 10/Fibanachi_0(n).py
@@ -0,0 +1,14 @@
+def fib_rec(n,arr):
+    if n <= 1:
+        return n
+    if (arr[n]) > 0:
+        return arr[n]
+    arr[n] = fib_rec(n-1,arr) + fib(n-2,arr)
+    return arr[n]
+
+def fib(n):
+    arr = [0]*(n+1)
+    arr[1]=1
+    return fib_rec(n,arr)
+
+print(fib(3))
