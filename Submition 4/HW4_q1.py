703dfd2963eaecd5695d513b477b2df413e6912e
Zer0Hiro <95877340+Zer0Hiro@users.noreply.github.com>
2025-01-21 21:32:08 +0200

    is_in function onptimize

--- Submition 4/HW4_q1.py
+++ Submition 4/HW4_q1.py
@@ -21,45 +21,50 @@ def is_palindrome(str, start, stop):
 def is_in(str, str_lst, index):
     # Starts to check from the end of the list
     num = len(str_lst) - 1
-    if num >= index:
-        if str == str_lst[num]:
-            return True
-        return is_in(str, str_lst[:num], index)
-    return False
+    if index == num:
+        return str == str_lst[index]
+    if str == str_lst[index]:
+        return True
+    return is_in(str, str_lst, index + 1)
 
-#Additional function for num_of_palindromes
-#Slice with specific size
+
+# Additional function for num_of_palindromes
+# Slice with specific size
 def palindrome_runner(str, start, end, lst):
-    #If end gets to the length of str
-    #Then it's out of index range => return lst of the function
+    # If end gets to the length of str
+    # Then it's out of index range => return lst of the function
     if end == len(str):
         return lst
-    #If it's palindrome and is not in the list
-    #Add to the list
+    # If it's palindrome and is not in the list
+    # Add to the list
     if is_palindrome(str, start, end):
         temp = str[start:end + 1]
         if not is_in(temp, lst, 0):
             lst.append(temp)
     return palindrome_runner(str, start + 1, end + 1, lst)
 
-#Creates size of scan for palindrome_runner
+
+# Creates size of scan for palindrome_runner
 def num_of_palindromes(str, start, end, pali_lst):
-    #When end is as big as string returns the size of the list
-    #list consists under palindrome parts
+    # When end is as big as string returns the size of the list
+    # list consists under palindrome parts
     if end == len(str):
         return len(pali_lst)
     pali_lst = palindrome_runner(str, start, end, pali_lst)
     return num_of_palindromes(str, start, end + 1, pali_lst)
 
 
+# Creates a list of str inputs
 def recursive_sts_lst_input(lst):
     str = input()
+    # Stops with empty string
     if str == "":
         return lst
     lst.append(str)
     return recursive_sts_lst_input(lst)
 
 
+# Main function
 def main():
     lst = []
     print("Please enter strings, separated by new lines. Enter an empty string for stop inputing: ")
