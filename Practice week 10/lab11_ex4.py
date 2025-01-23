5d47b5c3bdba6475280bf0e8c6aac1f466dc9679
Zer0Hiro <95877340+Zer0Hiro@users.noreply.github.com>
2025-01-23 19:07:28 +0200

    Update lab11_ex4.py

--- Practice week 10/lab11_ex4.py
+++ Practice week 10/lab11_ex4.py
@@ -2,16 +2,18 @@
 def check_num(str):
     if len(str) == 0:
         return 0
-    #String to number using ASCII decimal
+    # String to number using ASCII decimal
     num = ord(str[len(str) - 1]) - ord("0")
     return num + 10 * check_num(str[:len(str) - 1])
 
-#Check if there is only numbers
+
+# Check if there is only numbers
 def legal(str):
     for i in str:
         if not 48 <= ord(i) <= 57:
             return False
     return True
+
 
 # Gets the string from user
 # Print if the string is legal or illegal
@@ -19,8 +21,9 @@ def legal(str):
 def main():
     str = input("Please enter a string:")
     if legal(str):
-        print('The string "%s" corresponds to the number %d.' %(str, check_num(str)))
+        print('The string "%s" corresponds to the number %d.' % (str, check_num(str)))
     else:
         print("The string is illegal!")
+
 
 main()
