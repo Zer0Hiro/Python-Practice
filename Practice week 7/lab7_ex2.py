009521bf59ce09ff82454fc56a29d516eab14f5c
Zer0Hiro <95877340+Zer0Hiro@users.noreply.github.com>
2025-01-25 17:36:19 +0200

    Refactor

--- Practice week 7/lab7_ex2.py
+++ Practice week 7/lab7_ex2.py
@@ -2,28 +2,32 @@ def time_list(n):
     tlst = []
     for i in range(n):
         print()
-        tlst.append(float(input("%d. Enter time: "%(i+1))))
+        tlst.append(float(input("%d. Enter time: " % (i + 1))))
     return tlst
 
-def my_avg(lst,n):
+
+def my_avg(lst, n):
     avg = 0
     for i in range(n):
         avg += lst[i]
     avg /= len(lst)
     return avg
 
-def below(lst,avg):
+
+def below(lst, avg):
     count = 0
     for i in range(len(lst)):
         if lst[i] < avg:
             count += 1
-    print("The number of runners, running below average time is %d."%count)
-    
+    print("The number of runners, running below average time is %d." % count)
+
+
 def main():
     runners = int(input("Enter number of runners: "))
     t_lst = time_list(runners)
-    t_avg = my_avg(t_lst,runners)
-    print("Time average is %.2f."%(t_avg))
-    below(t_lst,t_avg)
-    
+    t_avg = my_avg(t_lst, runners)
+    print("Time average is %.2f." % (t_avg))
+    below(t_lst, t_avg)
+
+
 main()
