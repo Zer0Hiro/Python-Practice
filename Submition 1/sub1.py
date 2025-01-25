efd9e3319f503384191573aaeaa153cc435bf056
Zer0Hiro <95877340+Zer0Hiro@users.noreply.github.com>
2025-01-25 17:42:40 +0200

    Refactor

--- Submition 1/sub1.py
+++ Submition 1/sub1.py
@@ -1,8 +1,8 @@
-#Input checker every < 0 except -1 "Illegal data!"
-#Counter if delta is the same 
+# Input checker every < 0 except -1 "Illegal data!"
+# Counter if delta is the same
 
-#Dmitry Chybisov 342744869
-#Yelyzaveta Kytaieva 342794039
+# Dmitry Chybisov 342744869
+# Yelyzaveta Kytaieva 342794039
 def main():
     delta = 0
     saved_delta = 0
@@ -11,7 +11,7 @@ def main():
     start_check = 0
     delta_check = 0
     counter = 0
-    #Get first number and check if this possible
+    # Get first number and check if this possible
     firstnum = int(input("Insert 1st number: "))
     if firstnum <= 0:
         print("Illegal data!")
@@ -20,35 +20,37 @@ def main():
         if secondnum != -1 and secondnum > 0:
             length += 1
         start = firstnum
-        #Series checker
+        # Series checker
         while secondnum != -1 and secondnum > 0:
-            #If delta is True
+            # If delta is True
             delta = secondnum - firstnum
-            #Information storage
+            # Information storage
             if delta != saved_delta:
                 if length_check < length:
-                    length_check = length-1
+                    length_check = length - 1
                     delta_check = saved_delta
                     start_check = start
                 length = 2
                 start = firstnum
-            #Save previous variables for the next loop
+            # Save previous variables for the next loop
             saved_delta = delta
             firstnum = secondnum
             secondnum = int(input("Insert next number, end with -1: "))
             counter += 1
             if secondnum != -1 and secondnum > 0:
                 length += 1
-        #Check which series is bigger?   
+        # Check which series is bigger?
         if length_check >= length:
             start = start_check
             length = length_check
             saved_delta = delta_check
-        #Results  
+        # Results
         if secondnum <= 0 and secondnum != -1:
-            print("Illegal data!")   
+            print("Illegal data!")
         elif counter == 0:
-            print("Longest mathematical series start with %d, length of %d"%(start,length))
+            print("Longest mathematical series start with %d, length of %d" % (start, length))
         else:
-            print("Longest mathematical series start with %d, delta of %d, length of %d"%(start,saved_delta,length))              
+            print("Longest mathematical series start with %d, delta of %d, length of %d" % (start, saved_delta, length))
+
+
 main()
--- Submition 3/Matrix.py
+++ Submition 3/Matrix.py
@@ -1,64 +1,69 @@
-#Dmitry Chybisov 342744869
-#Yelyzaveta Kytaieva 342794039
+# Dmitry Chybisov 342744869
+# Yelyzaveta Kytaieva 342794039
 
 #    row       col
-#ar[index][under index]
+# ar[index][under index]
 
-#Check sub-matrix in specific coordinates
-def an_occurence(mat1,mat2,row,col):
+# Check sub-matrix in specific coordinates
+def an_occurence(mat1, mat2, row, col):
     for r in range(len(mat2)):
         for c in range(len(mat2[0])):
-            if mat1[r+row][c+col] != mat2[r][c]:
+            if mat1[r + row][c + col] != mat2[r][c]:
                 return False
     return True
 
-#Print matrix in specific range
-def print_sub(mat,row,col):
+
+# Print matrix in specific range
+def print_sub(mat, row, col):
     for r in range(row):
         for c in range(col):
-            print(mat[r][c], end = " ")
+            print(mat[r][c], end=" ")
         print()
 
-#Check if each part of main matrix devided by row/col will be the same
-def is_periodic(mat,row,col):
-    #Create matrix 2
+
+# Check if each part of main matrix devided by row/col will be the same
+def is_periodic(mat, row, col):
+    # Create matrix 2
     mat2 = []
     flag = True
-    #Create sub matrix
+    # Create sub matrix
     for ir in range(row):
         temp_mat = []
         for ic in range(col):
             temp_mat.append(mat[ir][ic])
         mat2.append(temp_mat)
-    #Check if sub matrix equal to the closest sub matrix
-    for r in range(0,len(mat),row):
-        for c in range(0,len(mat[0]),col):
+    # Check if sub matrix equal to the closest sub matrix
+    for r in range(0, len(mat), row):
+        for c in range(0, len(mat[0]), col):
             if flag != an_occurence(mat, mat2, r, c):
                 return False
         return True
-    
+
+
 def main():
-   row,col = eval((input("Enter matrix dimensions: ")))
-   mat = []
-   bestr = 0
-   bestc = 0
-   true = 0
-   print("Enter %d elements"%(row*col))
-   for r in range(row):
-       temp = []
-       for c in range(col):
-           temp.append(input())
-       mat.append(temp)
-   #Start to check if its periodic
-   for xr in range(1,len(mat)-1):
-       for xc in range(1,len(mat[0])-1):
-           if is_periodic(mat, xr, xc) != False:
-               bestr = xr
-               bestc = xc
-               true = 1
-   if true == 1:
-       print("The matrix is periodic.")
-       print("The sub-matrix is:")
-       return print_sub(mat, bestr, bestc)
-   return print("The matrix is not periodic.")
+    row, col = eval(input("Enter matrix dimensions: "))
+    mat = []
+    bestr = 0
+    bestc = 0
+    true = 0
+    print("Enter %d elements" % (row * col))
+    for r in range(row):
+        temp = []
+        for c in range(col):
+            temp.append(input())
+        mat.append(temp)
+    # Start to check if its periodic
+    for xr in range(1, len(mat) - 1):
+        for xc in range(1, len(mat[0]) - 1):
+            if is_periodic(mat, xr, xc) != False:
+                bestr = xr
+                bestc = xc
+                true = 1
+    if true == 1:
+        print("The matrix is periodic.")
+        print("The sub-matrix is:")
+        return print_sub(mat, bestr, bestc)
+    return print("The matrix is not periodic.")
+
+
 main()
