88b81cc1d38819293db75622cdda4fd50fab75c6
Zer0Hiro <95877340+Zer0Hiro@users.noreply.github.com>
2025-01-27 10:12:27 +0200

    lesson 11

--- Lesson 11/SortFunctions.py
+++ Lesson 11/SortFunctions.py
@@ -5,6 +5,7 @@ def bubble(lst):
             if lst[i] > lst[i + 1]:
                 lst[i], lst[i + 1] = lst[i + 1], lst[i]
 
+
 # Find the biggest number and put it at the end of list
 def max_sort(lst):
     n = len(lst)
@@ -15,6 +16,7 @@ def max_sort(lst):
                 maximum = x
         lst[n - 1 - i], lst[maximum] = lst[maximum], lst[n - 1 - i]
     return lst
+
 
 # Sorted list expansion
 def insertionSort(lst):
@@ -28,4 +30,30 @@ def insertionSort(lst):
     return lst
 
 
-print(insertionSort([1, 5, 2, 7, 3, 9]))
+#########################################
+# Connect 2 sorted lists
+
+def lstAppend(lst, result):
+    for i in range(len(lst)):
+        result.append(lst[i])
+    return result
+
+
+def sumlst(lst1, lst2):
+    result = []
+    x, y = 0,0
+    while x < len(lst1) and y < len(lst2):
+        if lst1[x] < lst2[y]:
+            result.append(lst1[x])
+            x += 1
+        else:
+            result.append(lst2[y])
+            y += 1
+    if x == len(lst1):
+        lstAppend(lst2[y:], result)
+    else:
+        lstAppend(lst1[x:], result)
+    return result
+
+
+print(sumlst([2, 5, 6], [1, 3, 5, 7, 8, 11]))
