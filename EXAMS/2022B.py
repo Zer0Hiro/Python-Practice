3c97f595f3f449c843f4b7ecb52e0841e1c30131
Zer0Hiro <95877340+Zer0Hiro@users.noreply.github.com>
2025-02-19 14:55:38 +0200

    Refactor

--- EXAMS/2022B.py
+++ EXAMS/2022B.py
@@ -21,3 +21,40 @@ def is_ab_mat(mat):
 # ---------------------------------------------------------
 # QUESTION 3
 
+def rec_prefix(s,pref):
+    if len(pref) == 0 or len(s) == 0:
+        return 0
+    if pref[0] == s[0]:
+        return  1 + rec_prefix(s[1:], pref[1:])
+    else:
+        return 0
+
+# print(rec_prefix("","aby"))
+
+def cntMaxSub(s,sub):
+    temp = 0
+    count = 0
+    for i in range(len(s)):
+        temp = rec_prefix(s[i:],sub)
+        if temp > count:
+            count = temp
+    return count
+
+# print(cntMaxSub("uabacad","abcdefg"))
+
+# ---------------------------------------------------------
+# QUESTION 4
+
+def is_packed(lst):
+    mx = max(lst)
+    mn = min(lst)
+    testl = []
+    for x in range(mn,mx+1):
+        testl.append(x)
+    for num in testl:
+        if num not in lst:
+            return False
+    return True
+
+print(is_packed([2,-1, -2, 4, 1, 0, 2]))
+
